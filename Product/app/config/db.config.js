a25b4359dab292320f5a7103e1babcdd92648944
mcch005 <martinchanch01@gmail.com>
2023-07-05 13:31:38 -0400

    first commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+url: "mongodb+srv://martinchanch01:znXhC27Qhzw1pJ6e@cluster0.ymzieob.mongodb.net/DressStore?retryWrites=true&w=majority"    
+};
